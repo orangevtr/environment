9518731ce2c0dc028f518485aece2754b5f92a3c
Takayuki Hirayama <htakayu1975@gmail.com>
2010-12-13 22:50:18 +0900

    vimプラグインセットアップ用shスクリプト

---
+++ setup-vim-bundles.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git clone git://github.com/Shougo/neocomplcache.git .vim/bundle/vim-neocomplcache
+git clone git://github.com/hotchpotch/perldoc-vim.git .vim/bundle/vim-perldoc
+git clone git://github.com/Shougo/unite.vim.git .vim/bundle/vim-unite
